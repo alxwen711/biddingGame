72c3a228f86641a906d8c1ac78cf16fc1396294f
alxwen711 <alxwen711@gmail.com>
2022-09-19 21:01:52 -0700

    mainpanel outline

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        new Screen();
+        Screen visual = new Screen();
     }
 }
---
+++ src/main/java/MainPanel.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel implements MouseListener {
+    public void paintComponent(Graphics g){
+
+    }
+
+    @Override
+    public void paint(Graphics g){
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
+
